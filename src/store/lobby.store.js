7da415eebc95fb4c8ec7edaa502162da3f3ad21e
Ryan <mrryanjohnston@gmail.com>
2022-05-13 13:42:58 -0400

    Update lobby store

--- src/store/lobby.store.js
+++ src/store/lobby.store.js
@@ -22,6 +22,12 @@ const useLobbyStore = defineStore("lobby", {
       let [fn, ...params] = data.split(' ');
       console.debug("function:", fn, "params:", params);
       this[fn](...params);
+      (this[fn] || this.debug(fn))(...params);
+    },
+    debug(fn) {
+      return function(...params) {
+	console.debug("Dropping call to", fn, params, "from game store")
+      };
     },
     refresh(socket) {
       this.games = [];
